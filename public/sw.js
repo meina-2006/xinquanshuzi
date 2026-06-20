0c57510024f394f32dec9cd489c086214d9d9353
meina-2006 <meina-2006@users.noreply.github.com>
2026-06-20 20:38:48 +0800

    feat: 添加PWA支持 - manifest.json + service worker + 离线缓存

---
+++ public/sw.js
@@ -0,0 +1,40 @@
+const CACHE = 'xinquan-v1';
+
+// 安装时缓存核心资源
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => {
+      return cache.addAll([
+        '/',
+        '/favicon.svg',
+        '/manifest.json'
+      ]);
+    })
+  );
+  self.skipWaiting();
+});
+
+// 激活后清理旧缓存
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE).map((key) => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// 网络优先，离线时降级到缓存
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const clone = response.clone();
+        caches.open(CACHE).then((cache) => cache.put(event.request, clone));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
